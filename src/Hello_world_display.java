4e66b087b72a9df48a3a0eaea594098852a93a9a
Daewon KIM <Daewon KIM@DESKTOP-91TFP8B>
2021-06-09 13:14:42 +0900

    add "add1" message

--- src/Hello_world_display.java
+++ src/Hello_world_display.java
@@ -3,5 +3,6 @@ public class Hello_world_display {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hi World");
+		System.out.println("add1");
 	}
 }
